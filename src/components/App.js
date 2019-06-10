20c4390def6e39cf06f3cf9ffc90d4d4c365d2d6
ilavisharma <lavisharma796@gmail.com>
2019-06-10 18:20:44 +0530

    add bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import firebase from '../lib/firebase';
+import Navbar from './Navbar';
 
 class App extends React.Component {
-  componentDidMount() {
-    const ref = firebase.firestore().collection('products');
-    ref
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          console.log(doc.data());
-        });
-      })
-      .catch(err => {
-        console.log('Error getting documents', err);
-      });
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <div>
+        <Navbar />
         <div>Hello</div>
       </div>
     );
