37f6904d0da57e0ff4ad718639949ae41b9d8df1
ilavisharma <lavisharma796@gmail.com>
2019-06-15 10:06:44 +0530

    card ui done

--- src/components/Home/CardDeck.js
+++ src/components/Home/CardDeck.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import Card from './Card';
+// import Card from './Card';
 import { connect } from 'react-redux';
 import { fetchProducts } from '../../actions';
 import firebase from '../../lib/firebase';
+import { ProductCard } from 'react-ui-cards';
+import './CardDeck.css';
 
 class CardDeck extends React.Component {
   componentDidMount() {
@@ -22,22 +24,35 @@ class CardDeck extends React.Component {
 
   renderCards = () => {
     const { products } = this.props;
+
     if (products !== []) {
       return products.map(product => {
-        return <Card product={product} key={product.id} />;
+        // return <Card product={product} key={product.id} />;
+        return (
+          <ProductCard
+            photos={product.image}
+            price={`Rs ${product.price}`}
+            productName={product.name}
+            buttonText="Add to Cart"
+            key={product.id}
+          />
+        );
       });
     }
   };
 
   render() {
     return (
-      <div className="container-fluid">
-        <div
-          className="row text-center d-flex"
-          style={{ display: 'flex', flexWrap: 'wrap' }}
-        >
-          {this.renderCards()}
-        </div>
+      // <div className="container-fluid">
+      //   <div
+      //     className="row text-center d-flex"
+      //     style={{ display: 'flex', flexWrap: 'wrap' }}
+      //   >
+      //     {this.renderCards()}
+      //   </div>
+      // </div>
+      <div className="">
+        <div className="card-container">{this.renderCards()}</div>
       </div>
     );
   }
