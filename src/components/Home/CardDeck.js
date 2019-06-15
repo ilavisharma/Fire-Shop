b0b9730207fbbc2aef428107e683388f004564ba
ilavisharma <lavisharma796@gmail.com>
2019-06-15 10:11:22 +0530

    remove Card import

--- src/components/Home/CardDeck.js
+++ src/components/Home/CardDeck.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Card from './Card';
 import { connect } from 'react-redux';
 import { fetchProducts } from '../../actions';
 import firebase from '../../lib/firebase';
