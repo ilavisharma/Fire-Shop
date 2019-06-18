366b88629237706988431795ba11cf7e9ba7a0a8
ilavisharma <lavisharma796@gmail.com>
2019-06-18 11:38:06 +0530

    fixed classname warnings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,32 +10,32 @@ class Navbar extends React.Component {
     return (
       <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
         <h5 className="my-0 mr-md-auto font-weight-normal">
-          <i style={iconStyle} class="material-icons">
+          <i style={iconStyle} className="material-icons">
             music_note
           </i>
           Guitar Shop
         </h5>
         <nav className="my-2 my-md-0 mr-md-3">
           <Link className="p-2 text-dark" to="/">
-            <i style={iconStyle} class="material-icons">
+            <i style={iconStyle} className="material-icons">
               home
             </i>
             Home
           </Link>
           <Link className="p-2 text-dark" to="/about">
-            <i style={iconStyle} class="material-icons">
+            <i style={iconStyle} className="material-icons">
               info
             </i>
             About
           </Link>
-          <Link className="p-2 text-dark" href="#">
-            <i style={iconStyle} class="material-icons">
+          <Link className="p-2 text-dark" to="#">
+            <i style={iconStyle} className="material-icons">
               shopping_cart
             </i>
             Cart
           </Link>
           <Link className="p-2 text-dark" to="/myaccount">
-            <i style={iconStyle} class="material-icons">
+            <i style={iconStyle} className="material-icons">
               account_circle
             </i>
             Account
