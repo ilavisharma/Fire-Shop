f4045235539dc80c3cfcf471d3d6fed08b9ccb07
ilavisharma <lavisharma796@gmail.com>
2019-06-22 18:37:15 +0530

    items are noe shoeing up

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import {} from '../actions';
 
-const Cart =() => {
+const Cart = props => {
+  const renderList = () =>
+    props.cart.map(item => <li className="list-group-item">{item.name}</li>);
+
+  if (props.cart.length !== 0) {
     return (
-        <div>
-            <h1>This the cart</h1>
-        </div>
-    )
-}
+      <div className="container">
+        {/* Helmet info */}
+        <Helmet>
+          <title>{`${props.cart.length} items in cart`}</title>
+        </Helmet>
+        <ul className="list-group list-group-flush">{renderList()}</ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        {/* Helmet info */}
+        <Helmet>
+          <title>Empty Cart</title>
+        </Helmet>
+        <h3 className="display-3">Cart is empty</h3>
+      </div>
+    );
+  }
+};
 
-export default Cart
+const mapStateToProps = state => {
+  return {
+    cart: state.cart
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(Cart);
