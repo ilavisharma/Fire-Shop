c22bdce8353a1ff45e8e245b5eb98e284802e81e
ilavisharma <lavisharma796@gmail.com>
2019-06-22 16:08:58 +0530

    google auth is working now

--- src/components/Authentication/GoogleAuth.js
+++ src/components/Authentication/GoogleAuth.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import './AuthButton.css';
+import firebase from '../../lib/firebase';
+import { toast } from 'react-toastify';
+import { connect } from 'react-redux';
+import { signIn } from '../../actions';
 
 class GoogleAuth extends Component {
+  onButtonClick = async () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    try {
+      const result = await firebase.auth().signInWithPopup(provider);
+      const { user } = result;
+      this.props.signIn(user);
+    } catch (e) {
+      console.log(e);
+      toast.error(e.message);
+    }
+  };
+
   render() {
     return (
-      <button className="loginBtn loginBtn--google">Login with Google</button>
+      <button
+        onClick={this.onButtonClick}
+        className="loginBtn loginBtn--google"
+      >
+        Login with Google
+      </button>
     );
   }
 }
 
-export default GoogleAuth;
+export default connect(
+  null,
+  { signIn }
+)(GoogleAuth);
