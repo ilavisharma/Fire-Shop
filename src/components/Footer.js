16b33027dc337046f45f01927bb1be26ac2125dc
ilavisharma <lavisharma796@gmail.com>
2019-06-24 20:35:05 +0530

    create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <footer
+        style={{
+          backgroundColor: '#3f3f3f',
+          color: '#d5d5d5',
+          paddingTop: '2rem'
+        }}
+      />
+    </div>
+  );
+};
+
+export default Footer;
