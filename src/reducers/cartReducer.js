5daefa6d07d8ef7121c7804517b2b8d301daff9e
ilavisharma <lavisharma796@gmail.com>
2019-06-25 12:07:40 +0530

    inc and dec item quantity

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -4,6 +4,25 @@ export default (state = [], action) => {
       return [...state, action.payload];
     case 'REMOVE_FROM_CART':
       return state.filter(item => item !== action.payload);
+    case 'INCREMENT':
+      let newArray = [];
+      state.forEach(item => {
+        if (item.id === action.payload.id) {
+          item.quantity++;
+          newArray.push(item);
+        } else newArray.push(item);
+      });
+      return newArray;
+    case 'DECREMENT':
+      let newCart = [];
+      state.forEach(item => {
+        if (item.id === action.payload.id) {
+          item.quantity--;
+          newCart.push(item);
+        } else newCart.push(item);
+      });
+      return newCart;
+
     default:
       return state;
   }
